996ec86677e7e469f6495b2a29ac8a58b417910b
auston salvana <austonsalvana@austons-macbook-pro.local>
2016-09-24 14:00:43 -0700

    Finished swipe functionality and yes or no button. Will integrate Firebase SDK

--- RateMyBrand/ViewController.swift
+++ RateMyBrand/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import Koloda
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kolodaView: KolodaView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: IBActions
+    @IBAction func leftButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Left)
+    }
+    
+    @IBAction func rightButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Right)
+    }
+    
+    @IBAction func undoButtonTapped() {
+        kolodaView?.revertAction()
+    }
 
 
 }
 
+extension ViewController: KolodaViewDelegate {
+    func kolodaDidRunOutOfCards(koloda: KolodaView) {
+        
+    }
+    
+    func koloda(koloda: KolodaView, didSelectCardAtIndex index: UInt) {
+//        UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
+    }
+}
+
+extension ViewController: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(koloda:KolodaView) -> UInt {
+        return 20
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardAtIndex index: UInt) -> UIView {
+        return UIImageView(image: UIImage(named: "\(index)"))
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
+        return NSBundle.mainBundle().loadNibNamed("OverlayView",
+                                                  owner: self, options: nil)[0] as? OverlayView
+    }
+}
