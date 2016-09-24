2290284209b3b3bef4e84fd84334e5a330825793
auston salvana <austonsalvana@austons-macbook-pro.local>
2016-09-23 22:12:07 -0700

    Initial Commit

---
+++ RateMyBrand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateMyBrand
+//
+//  Created by auston salvana on 2016-09-23.
+//  Copyright © 2016 ASolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
